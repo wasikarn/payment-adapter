d773a0f495f067437b07d01ae9ccfca362935432
BIG <bigguitarsolo@gmail.com>
2025-01-11 00:52:17 +0700

    Remove AppController and AppService from the application
    This commit deletes the AppController, AppService, and their
    corresponding test file. The app.module.ts file is also updated to
    remove references to these components. This simplifies the application
    structure by eliminating unused or unnecessary parts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
-  controllers: [AppController],
+  controllers: [],
   imports: [ConfigModule.forRoot({ expandVariables: true, isGlobal: true })],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
