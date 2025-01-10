4c7b8fe101ed22f4d109f06b7e95fd21e7c5975a
BIG <bigguitarsolo@gmail.com>
2025-01-11 01:47:46 +0700

    Add support for multiple payment gateways
    Introduced Stripe and PayPal gateways with dynamic selection based on
    configuration. Enhanced `PaymentService` to handle payments and refunds
    through the provided gateway. Added logging for improved visibility into
     payment and refund processes.

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 
+import { PaypalGateway } from './gateways/paypal.gateway';
+import { StripeGateway } from './gateways/stripe.gateway';
 import { PaymentService } from './payment.service';
 
 @Module({
-  providers: [PaymentService],
+  exports: [PaymentService],
+  providers: [
+    PaymentService,
+    StripeGateway,
+    PaypalGateway,
+    {
+      provide: 'PAYMENT_GATEWAY',
+      useClass:
+        process.env.PAYMENT_GATEWAY === 'paypal'
+          ? PaypalGateway
+          : StripeGateway,
+    },
+  ],
 })
 export class PaymentModule {}
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+
+import { IPaymentGateway } from './interfaces/payment-gateway.interface';
+import { IPaymentResult } from './interfaces/payment-result.interface';
+import { IRefundResult } from './interfaces/refund-result.interface';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  private readonly logger: Logger = new Logger(PaymentService.name);
+
+  constructor(
+    @Inject('PAYMENT_GATEWAY') private readonly paymentGateway: IPaymentGateway,
+  ) {}
+
+  async processOrderPayment(
+    amount: number,
+    currency: string,
+  ): Promise<IPaymentResult> {
+    const result: IPaymentResult = await this.paymentGateway.processPayment(
+      amount,
+      currency,
+    );
+
+    if (result.success) {
+      this.logger.log(
+        `Payment successful. Transaction ID: ${result.transactionId}`,
+      );
+    } else {
+      this.logger.error(`Payment failed: ${result.message}`);
+    }
+
+    return result;
+  }
+
+  async refundOrderPayment(transactionId: string): Promise<IRefundResult> {
+    const result: IRefundResult =
+      await this.paymentGateway.refundPayment(transactionId);
+
+    if (result.success) {
+      this.logger.log(`Refund successful. Transaction ID: ${transactionId}`);
+    } else {
+      this.logger.error(`Refund failed: ${result.message}`);
+    }
+
+    return result;
+  }
+}
